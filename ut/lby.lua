714118cbaf047defa4ed21def1129e243f4b90aa
lghft <121789924+lghft@users.noreply.github.com>
2025-04-02 16:01:18 +0000

    Update lby.lua

--- ut/lby.lua
+++ ut/lby.lua
@@ -12,18 +12,20 @@ if plrAmount == 1 and game.Players.LocalPlayer and game.Workspace.Lobby then
         char:MoveTo(tele.Position)
     end
 end
-spawn(function())
-while true do
-    if plrAmount > 1 and game.Workspace.Lobby then
-        if ptyFind.Visible == true then
-            wait(1)
-            wait(1)
-            local genServ = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.PartyFinder.Main.MyServerPanel.Main.Content.LastSavedServer.Panel.GenerateNewServerButton
-            firesignal(genServ.Activated)
-            wait(1)
-            local jlservB = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.PartyFinder.Main.MyServerPanel.Main.Content.LastSavedServer.Panel.Join
-            firesignal(jlservB.Activated)
-            wait(1)
+
+spawn(function()
+    while true do
+        if plrAmount > 1 and game.Workspace.Lobby then
+            if ptyFind.Visible == true then
+                wait(1)
+                wait(1)
+                local genServ = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.PartyFinder.Main.MyServerPanel.Main.Content.LastSavedServer.Panel.GenerateNewServerButton
+                firesignal(genServ.Activated)
+                wait(1)
+                local jlservB = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.PartyFinder.Main.MyServerPanel.Main.Content.LastSavedServer.Panel.Join
+                firesignal(jlservB.Activated)
+                wait(1)
+            end
         end
     end
-end()
+end)
