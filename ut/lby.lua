48ac5d3dc5e9646dd371c36b9b47ee5e87e97cb6
lghft <121789924+lghft@users.noreply.github.com>
2025-04-02 03:07:24 +0000

    Update lby.lua

--- ut/lby.lua
+++ ut/lby.lua
@@ -3,6 +3,7 @@ local char = game.Players.LocalPlayer.Character
 local Players = game:GetService('Players')
 local plrAmount = #Players:GetPlayers()
 local tele = workspace.Lobby.EventTeleporters:GetChildren()[2]["Cylinder.119"].VFX.hitbox
+local ptyFind = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.HUD.Main2.PartyFinder
 
 if plrAmount == 1 and game.Players.LocalPlayer and game.Workspace.Lobby then
     if tele then
