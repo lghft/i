fc42f4b6e01f7800dfce31c2c7f48d725573e0b6
lghft <121789924+lghft@users.noreply.github.com>
2025-04-04 05:43:00 +0000

    Update lby.lua

--- ut/lby.lua
+++ ut/lby.lua
@@ -1,15 +1,26 @@
 print("lby?")
 repeat wait(5) until game:IsLoaded()
 print("Lby Loaded1")
+local dun = true
+local eve = false
 local char = game.Players.LocalPlayer.Character
 local Players = game:GetService('Players')
 local plrAmount = #Players:GetPlayers()
 local tele = workspace.Lobby.EventTeleporters:GetChildren()[2]["Cylinder.119"].VFX.hitbox
+local dtele = workspace.Lobby.DungeonTeleporters.Teleporter2.Top
 local ptyFind = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.HUD.Main2.PartyFinder
 
 if plrAmount == 1 and game.Players.LocalPlayer and game.Workspace.Lobby then
-    if tele then
+    if tele and eve == true then
         char:MoveTo(tele.Position)
+    elseif dtele and dun == true then
+        char:MoveTo(dtele.Position)
+        repeat wait() until game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.FloorSelection.Visible == true
+        local hard = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.FloorSelection.SelectedMap.Buttons.HardcoreButton
+        firesignal(hard.Activated)
+        wait(0.5)
+        local strt = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.FloorSelection.SelectedMap.Buttons.StartButton
+        firesignal(strt.Activated)
     end
 elseif plrAmount > 1 and game.Players.LocalPlayer and not game.Workspace.Lobby then
     game:Shutdown()
@@ -17,7 +28,7 @@ elseif plrAmount > 1 and game.Workspace.Lobby then
     if ptyFind.Visible == true then
         local genServ = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.PartyFinder.Main.MyServerPanel.Main.Content.LastSavedServer.Panel.GenerateNewServerButton
         firesignal(genServ.Activated)
-        wait(1)
+        wait(0.5)
         local jlservB = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.PartyFinder.Main.MyServerPanel.Main.Content.LastSavedServer.Panel.Join
         firesignal(jlservB.Activated)
     end
