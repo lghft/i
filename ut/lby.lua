5067351e75202896c793f2a3b16c8fb09135f811
lghft <121789924+lghft@users.noreply.github.com>
2025-04-05 00:32:57 +0000

    Update lby.lua

--- ut/lby.lua
+++ ut/lby.lua
@@ -44,6 +44,9 @@ function autoclosesmtn()
     end
 end
 
+spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lghft/i/refs/heads/main/ut/rdr.lua"))()
+end)
 
 if plrAmount == 1 and game.Players.LocalPlayer and game.Workspace.Lobby then
     print("=1")
@@ -63,7 +66,6 @@ if plrAmount == 1 and game.Players.LocalPlayer and game.Workspace.Lobby then
         clickButton(strt)
     elseif story == true then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/Main.lua"))()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/lghft/i/refs/heads/main/ut/rdr.lua"))()
     end
 elseif plrAmount > 1 and game.Workspace.Lobby then
     print(">1")
