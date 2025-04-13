301795ebb3fac05f8858f6796a4c5bbf1204f05b
lghft <121789924+lghft@users.noreply.github.com>
2025-04-13 03:51:00 +0000

    Update sped.lua

--- aw/sped.lua
+++ aw/sped.lua
@@ -1,6 +1,15 @@
 repeat wait(4) until game:IsLoaded()
+function spd()
 local args = {
     [1] = "x2"
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("x2Event"):FireServer(unpack(args))
+repeat wait() until game:GetService("Players").LocalPlayer.PlayerGui.InterFace.Enabled == false
+    if game:GetService("Players").LocalPlayer.PlayerGui.InterFace.Enabled == false then
+        spd()
+    end
+end
+
+
+spd()
