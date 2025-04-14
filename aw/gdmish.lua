44faa9205f61785c8651372d31b133133106123c
lghft <121789924+lghft@users.noreply.github.com>
2025-04-14 00:49:48 +0000

    Create gdmish.lua

---
+++ aw/gdmish.lua
@@ -0,0 +1,547 @@
+
+function test()
+	
+local args = {
+    [1] = "Koolin",
+    [2] = CFrame.new(-107.4432144165039, 82.77709197998047, 7.699348449707031, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+wait(0.5)
+
+local args = {
+    [1] = "Koolin",
+    [2] = CFrame.new(-61.24794006347656, 82.77709197998047, -19.024085998535156, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+local args = {
+    [1] = "Koolin",
+    [2] = CFrame.new(-15.961631774902344, 82.77709197998047, -18.779312133789062, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "Koolin",
+    [2] = CFrame.new(18.64712142944336, 82.77709197998047, 11.066429138183594, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+
+local args = {
+    [1] = "Koolin",
+    [2] = CFrame.new(33.531341552734375, 82.77709197998047, 40.18518829345703, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "Koolin",
+    [2] = CFrame.new(50.8375244140625, 82.77709197998047, 65.25202941894531, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "GemGirl",
+    [2] = CFrame.new(31.84864044189453, 82.66156768798828, 93.01586151123047, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "GemGirl",
+    [2] = CFrame.new(-4.828922271728516, 82.66156768798828, 125.2080078125, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "GemGirl",
+    [2] = CFrame.new(-36.056732177734375, 82.66156768798828, 145.712158203125, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "GemGirl",
+    [2] = CFrame.new(-77.87692260742188, 82.66156768798828, 128.17236328125, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "GemGirl",
+    [2] = CFrame.new(-105.9616928100586, 82.66156768798828, 97.5626220703125, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "GemGirl",
+    [2] = CFrame.new(-119.37454223632812, 82.66156768798828, 50.74709701538086, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "GemGirl",
+    [2] = CFrame.new(-126.83667755126953, 82.66156768798828, 25.58090591430664, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "GemGirl",
+    [2] = CFrame.new(19.34088134765625, 82.66156768798828, 114.08220672607422, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+
+wait(0.5)
+
+
+local args = {
+    [1] = "Leader",
+    [2] = CFrame.new(-29.461856842041016, 82.77421569824219, 54.42395782470703, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Leader")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Leader")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Leader")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Leader")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Leader")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Leader")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+
+wait(0.5)
+
+wait(0.5)
+
+
+local args = {
+    [1] = "King of Heroes",
+    [2] = CFrame.new(-35.02606201171875, 82.79618835449219, 53.379058837890625, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "Butterfly Guard",
+    [2] = CFrame.new(-18.843151092529297, 82.76700592041016, 32.735633850097656, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "Peem",
+    [2] = CFrame.new(-28.53891372680664, 82.77709197998047, 61.09062194824219, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+    [3] = 1,
+    [4] = {
+        [1] = "1",
+        [2] = "1",
+        [3] = "1",
+        [4] = "1"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SpawnUnit"):InvokeServer(unpack(args))
+
+wait(0.5)
+
+local args = {
+    [1] = "King Treasure Ig-Alima",
+    [2] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UnitAbility"):FireServer(unpack(args))
+
+
+wait(0.5)
+
+local args = {
+    [1] = "King Treasure Ig-Alima",
+    [2] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UnitAbility"):FireServer(unpack(args))
+
+
+wait(0.5)
+
+wait(1)
+
+local args = {
+    [1] = "King Treasure Ig-Alima",
+    [2] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UnitAbility"):FireServer(unpack(args))
+
+
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("King of Heroes")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+
+wait(1)
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Peem")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Peem")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Peem")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Peem")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Peem")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Peem")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Peem")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+
+wait(1)
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Butterfly Guard")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Butterfly Guard")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Butterfly Guard")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Butterfly Guard")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Butterfly Guard")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Butterfly Guard")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Butterfly Guard")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = workspace:WaitForChild("Units"):WaitForChild("Butterfly Guard")
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("UpgradeUnit"):InvokeServer(unpack(args))
+
+wait(2)
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SkipEvent"):FireServer()
+
+repeat wait() until game:GetService("Players").LocalPlayer.PlayerGui.InterFace.Equip.val.Cash_Value.val.Text == "777777 $"
+test()
+end
+
+test()
