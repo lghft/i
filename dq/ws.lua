3abac914f3c523bbbf72a8c5f010d6128a63334a
lghft <121789924+lghft@users.noreply.github.com>
2025-05-11 16:32:17 +0000

    Update ws.lua

--- dq/ws.lua
+++ dq/ws.lua
@@ -1,5 +1,9 @@
+repeat wait(6) until game:IsLoaded()
+
 local plrs = game.Players:GetChildren()
 if #plrs == 1 then
+wait(4)
+    print("LOADED?")
     getgenv().active = true
     while getgenv().active == true do
     --[[
@@ -44,20 +48,16 @@ if #plrs == 1 then
         "80E25D5E-935D-44E3-8CAE-C0FEDE8E9F3F"
     }
     game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Chain Lightning"):WaitForChild("abilityEvent"):FireServer(unpack(args))
---[[
-    local args = {
-        {
-            {
-                ["\003"] = "vote",
-                vote = true
-            },
-            "/"
-        }
-    }
-    game:GetService("ReplicatedStorage"):WaitForChild("dataRemoteEvent"):FireServer(unpack(args))
-]]
+    local bnsBut = game:GetService("Players").LocalPlayer.PlayerGui.RetryVote.Frame.Bonus
+    local rplyBut = game:GetService("Players").LocalPlayer.PlayerGui.RetryVote.Frame.Retry
+    if bnsBut.Visible == true and rplyBut.Visible == true then
+    print("YeES")
+        firesignal(lbyBut.Activated)
+    elseif bnsBut.Visible == false and rplyBut.Visible == true then
+        firesignal(rplyBut.Activated)
+    end
     game:GetService("ReplicatedStorage"):WaitForChild("remotes"):WaitForChild("changeStartValue"):FireServer()
     wait()
-    game.Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid').WalkSpeed = 35
+    game.Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid').WalkSpeed = 33
     end
 end
