b782fb8a76d090046bec10e476f9dc28d42a0a74
lghft <121789924+lghft@users.noreply.github.com>
2025-05-11 00:44:29 +0000

    Update ws.lua

--- dq/ws.lua
+++ dq/ws.lua
@@ -2,14 +2,49 @@ local plrs = game.Players:GetChildren()
 if #plrs == 1 then
     getgenv().active = true
     while getgenv().active == true do
+    --[[
+    local args = {
+        "405BF493-86B1-4B2D-A281-2BBFF16F6F13"
+    }
+    game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Explosive Mine"):WaitForChild("spellEvent"):FireServer(unpack(args))
+    ]]
+
+    local args = {
+        "80E25D5E-935D-44E3-8CAE-C0FEDE8E9F3F"
+    }
     game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Chain Lightning"):WaitForChild("abilityEvent"):FireServer(unpack(args))
+
+    local args = {
+        "80E25D5E-935D-44E3-8CAE-C0FEDE8E9F3F"
+    }
     game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Chain Lightning"):WaitForChild("abilityEvent"):FireServer(unpack(args))
+
+    local args = {
+        "80E25D5E-935D-44E3-8CAE-C0FEDE8E9F3F"
+    }
     game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Chain Lightning"):WaitForChild("abilityEvent"):FireServer(unpack(args))
+
+    local args = {
+        "80E25D5E-935D-44E3-8CAE-C0FEDE8E9F3F"
+    }
     game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Chain Lightning"):WaitForChild("abilityEvent"):FireServer(unpack(args))
+    local args = {
+        "80E25D5E-935D-44E3-8CAE-C0FEDE8E9F3F"
+    }
     game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Chain Lightning"):WaitForChild("abilityEvent"):FireServer(unpack(args))
+    local args = {
+        "80E25D5E-935D-44E3-8CAE-C0FEDE8E9F3F"
+    }
     game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Chain Lightning"):WaitForChild("abilityEvent"):FireServer(unpack(args))
+    local args = {
+        "80E25D5E-935D-44E3-8CAE-C0FEDE8E9F3F"
+    }
     game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Chain Lightning"):WaitForChild("abilityEvent"):FireServer(unpack(args))
+    local args = {
+        "80E25D5E-935D-44E3-8CAE-C0FEDE8E9F3F"
+    }
     game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):WaitForChild("Chain Lightning"):WaitForChild("abilityEvent"):FireServer(unpack(args))
+
     local args = {
         {
             {
@@ -20,6 +55,7 @@ if #plrs == 1 then
         }
     }
     game:GetService("ReplicatedStorage"):WaitForChild("dataRemoteEvent"):FireServer(unpack(args))
+
     game:GetService("ReplicatedStorage"):WaitForChild("remotes"):WaitForChild("changeStartValue"):FireServer()
     wait()
     game.Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid').WalkSpeed = 35
