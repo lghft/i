816d1c2f7454cac71e3c87ca2823ac746c394113
lghft <121789924+lghft@users.noreply.github.com>
2025-05-13 00:52:54 +0000

    Update ws.lua

--- dq/ws.lua
+++ dq/ws.lua
@@ -37,7 +37,7 @@ if #plrs == 1 then
             local chainLightning = safeWaitForChild(backpack, "Chain Lightning", 10)
             local abilityEvent = safeWaitForChild(chainLightning, "abilityEvent", 10)
             local args1 = {"80E25D5E-935D-44E3-8CAE-C0FEDE8E9F3F"}
-
+            --[[
             local args = {
                 {
                     {
@@ -51,7 +51,7 @@ if #plrs == 1 then
             -- Handle retry button
             local rplyBut = game:GetService("Players").LocalPlayer.PlayerGui.RetryVote.Frame.Retry
             firesignal(rplyBut.Activated)
-                
+            ]]
             abilityEvent:FireServer(unpack(args1))
             abilityEvent:FireServer(unpack(args1))
             abilityEvent:FireServer(unpack(args1))
