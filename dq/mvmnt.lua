13c6624ddaa9007f6f4f87e9cc00d4fda5bde456
lghft <121789924+lghft@users.noreply.github.com>
2025-06-08 16:06:34 +0000

    Update mvmnt.lua

--- dq/mvmnt.lua
+++ dq/mvmnt.lua
@@ -578,5 +578,7 @@ if config.autoplay == true and config.selectedMacro == "Void3" then
     wait(5)
     clickButton(game.CoreGui.DqmacMacroGui.Frame.Play)
     wait(10)
+    clickButton(game.CoreGui.DqmacMacroGui.Frame.Play)
+    wait(1)
     gui.Enabled = false
 end
