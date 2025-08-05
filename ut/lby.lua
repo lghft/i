9fffc350046860225ac8327207e2d0c8dd769d5c
lghft <121789924+lghft@users.noreply.github.com>
2025-08-05 22:02:20 +0000

    Update lby.lua

--- ut/lby.lua
+++ ut/lby.lua
@@ -1,5 +1,6 @@
 print("lby?")
 repeat wait(5) until game:IsLoaded()
+repeat wait() until game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.LoadingScreen.Visible == false
 print("Lby Loaded1")
 local dun = true
 local eve = false
