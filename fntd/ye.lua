739e1c7f0112742b496e4b5c5df5b5748aa133fe
lghft <121789924+lghft@users.noreply.github.com>
2025-10-25 07:49:17 +0000

    Update ye.lua

--- fntd/ye.lua
+++ fntd/ye.lua
@@ -1,1 +1,20 @@
-print("hi")
+local args = {
+	{
+		PlaceCFrame = CFrame.new(-1489.8123779296875, 0.9685236215591431, -67.27490234375, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+		UnitGUID = "{8224b47b-987e-40c8-a12f-81d8e86b501c}"
+	}
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RE/PlaceUnit"):FireServer(unpack(args))
+
+
+local args = {
+	"{ad4537f4-e23b-4612-a91a-81f89966d309}"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RE/UpgradeUnit"):FireServer(unpack(args))
+
+
+local args = {
+	"SkipWave"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RE/VoteEvent"):FireServer(unpack(args))
+
