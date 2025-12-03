348ad188210ede7bbfb9dd72ce76c5d55cce91c6
lghft <121789924+lghft@users.noreply.github.com>
2025-12-02 19:21:46 -0800

    Update events.lua

--- utd/events.lua
+++ utd/events.lua
@@ -1,5 +1,5 @@
 repeat wait(1) until game:IsLoaded()
-repeat wait(1) until game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.LoadingScreen.Visible == false
+repeat wait() until game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.LoadingScreen.Visible == false
 print("loaded evnts")
 local bulmaT = {}
 local remote = game.ReplicatedStorage.Modules.GlobalInit.RemoteEvents.PlayerActivateTowerAbility
