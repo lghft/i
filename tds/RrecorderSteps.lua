d1300620b4a4c20bd53aa7d64b4860a6a20a2a63
lghft <121789924+lghft@users.noreply.github.com>
2025-12-26 21:50:54 -0800

    Create RrecorderSteps.lua

---
+++ tds/RrecorderSteps.lua
@@ -0,0 +1,7 @@
+
+### 🛠️ How to Use
+
+1. **Start the Game:** Once you reach the "Ready" UI, click Start and begin the match.
+2. **Record Your Gameplay:** Place your towers and play through the game normally (or follow a YouTube guide).
+3. **Save Your Strategy:** Once you win or lose, click **Stop**.
+4. **Retrieve Your Script:** Head to your Delta file directory and open the `workspace` folder. You’ll find a file named `Strat.txt`—that’s your completed strategy!
