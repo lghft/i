09eea81b4eebed5ed73fe8d538004ae170286287
lghft <121789924+lghft@users.noreply.github.com>
2025-12-30 17:14:41 -0800

    Update D.lua

--- dex/d1/D.lua
+++ dex/d1/D.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lghft/i/refs/heads/main/dex/dd.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lghft/i/refs/heads/main/dex/d1/dd.lua", true))()
