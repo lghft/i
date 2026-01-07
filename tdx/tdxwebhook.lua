a16bcf11857d810043bb53fc767a8dc3a96236bd
lghft <121789924+lghft@users.noreply.github.com>
2026-01-06 20:09:39 -0800

    Update Webhook URL in tdxwebhook.lua

--- tdx/tdxwebhook.lua
+++ tdx/tdxwebhook.lua
@@ -1,3 +1,3 @@
-getgenv().Webhook = "WebHookHere"
+getgenv().Webhook = "https://discord.com/api/webhooks/1414475374557007912/a6EAVcQA2FUYMftYLUTklmDxDYiVvojfinLtUdcJzbZG65jVkI2BO5VXUIDKSP1NRv2S"
 getgenv().SendMatchResult = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/lghft/i/refs/heads/main/tdx/tdxwebhook.lua"))()
