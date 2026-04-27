08bb22a981399fee63397626c92906946a54b5fa
lghft <121789924+lghft@users.noreply.github.com>
2026-04-26 21:22:42 -0700

    Update Vipserverjoiner.lua

--- PlayerUtil/Vipserverjoiner.lua
+++ PlayerUtil/Vipserverjoiner.lua
@@ -1,10 +1,14 @@
-local ServerType = game:GetService('RobloxReplicatedStorage').GetServerType:InvokeServer()
-local linkCode = "";
-if ServerType ~= "VIPServer" then
-    local args = {
-        placeId = game.PlaceId, 
-        linkCode = tostring(code)
-    }
-    game:GetService("ExperienceService"):LaunchExperience(args)
-    return true
+local code = "66632761641938203785126344046015"
+
+if code ~= nil and not MarketplaceService:UserOwnsGamePassAsync(LocalPlayer.UserId, 10518590) then
+    local ServerType = game:GetService('RobloxReplicatedStorage').GetServerType:InvokeServer()
+    
+    if ServerType ~= "VIPServer" then
+        local args = {
+            placeId = game.PlaceId, 
+            linkCode = tostring(code)
+        }
+        game:GetService("ExperienceService"):LaunchExperience(args)
+        return true
+    end
 end
