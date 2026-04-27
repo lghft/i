9aa35090d0ae88e4786000b8561ff3271eec8010
lghft <121789924+lghft@users.noreply.github.com>
2026-04-26 22:10:29 -0700

    Update Vipserverjoiner.lua

--- PlayerUtil/Vipserverjoiner.lua
+++ PlayerUtil/Vipserverjoiner.lua
@@ -1,6 +1,6 @@
 local code = "66632761641938203785126344046015"
 
-if code ~= nil and not MarketplaceService:UserOwnsGamePassAsync(LocalPlayer.UserId, 10518590) then
+if code ~= nil then
     local ServerType = game:GetService('RobloxReplicatedStorage').GetServerType:InvokeServer()
     
     if ServerType ~= "VIPServer" then
